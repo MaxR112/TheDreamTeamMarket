4a46227e65d2d2a69347a52a5fd92d55742097b9
Max Raymond <maxr9090@gmail.com>
2025-04-19 10:30:18 -0400

    feat: Create checkForFileAndCreateIfNotPresent()

--- src/main/java/DreamTeam/fileHandler/fileWriter.java
+++ src/main/java/DreamTeam/fileHandler/fileWriter.java
@@ -1,5 +1,7 @@
 package main.java.DreamTeam.fileHandler;
 
+import java.io.File;
+import java.io.IOException;
 import java.util.ArrayList;
 import main.java.DreamTeam.Products.Product;
 
@@ -10,6 +12,22 @@ public class fileWriter{
     public fileWriter(ArrayList<Product> productList){
         this.allProductsToWrite = new ArrayList<>(productList);
         System.out.println("Test: Successfully created fileWriter instance");
+    }
+
+    public void writeProductsToFile(){
+        checkForFileAndCreateIfNotPresent();
+        
+    }
+
+    private void checkForFileAndCreateIfNotPresent() {
+        try {
+            File productFile = new File("allProductCatalog.txt");
+            if (productFile.createNewFile()){
+                System.out.println("Product file created: " + productFile.getName());
+            }
+        } catch (IOException e) {
+            System.out.println("File error " + e);
+        }
     }
 
     
