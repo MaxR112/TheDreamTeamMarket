ecd67acdc4d19aa8ee1fd3777592e401cd9900a8
Max Raymond <maxr9090@gmail.com>
2025-04-18 23:27:47 -0400

    fix: Reformat the constructors for test products

--- src/main/java/DreamTeam/Driver/Driver.java
+++ src/main/java/DreamTeam/Driver/Driver.java
@@ -14,28 +14,17 @@ public class Driver{
         //Some code is commented out as implementation does not exist on this branch.
         ArrayList<Product> sampleProducts = new ArrayList<>();
 
-        Clothing shirt = new Clothing("M", "Blue", "Casual");
-        shirt.setName("T-Shirt");
-        shirt.setPrice(19.99);
-        shirt.setQuantity(10);
-        shirt.setCompany("CottonCo");
-        shirt.setDescription("100% Cotton T-Shirt");
-        //shirt.setIdentifier("C001");
+        Clothing shirt = new Clothing("Aqua-Man Shirt", "C001", 19.99, 10, "Soft, blue Aqua-Man Shirt", "CottonCo", "Large", "T-Shirt", "Blue");
 
-        Electronics laptop = new Electronics(120, 2.5, "Black");
-        laptop.setName("Laptop X15");
-        laptop.setPrice(999.99);
-        laptop.setQuantity(5);
-        laptop.setCompany("TechWorld");
-        laptop.setDescription("15-inch gaming laptop");
-        laptop.setIdentifier("E001");
+        Electronics laptop = new Electronics("Dell X15", "E001", 999.99, 5, "15-inch gaming laptop", "Dell", "350 watts", "12.3 pounds", "Dark Gray");
+
 
         sampleProducts.add(shirt);
         sampleProducts.add(laptop);
 
-        Catalog catalog = new Catalog();
-        catalog.setCatalog(sampleProducts);
-        catalog.displayAllCatalog();
+        setCatalog catalog = new setCatalog();
+        //catalog.setCatalog(sampleProducts);
+        //catalog.displayAllCatalog();
 
         //Run the screen operation on a seperate thread.
         javax.swing.SwingUtilities.invokeLater(new Runnable() {
